54a4a40b3f81c5a68e73f0abbebc8cd055e4b710
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-10-21 23:46:13 +0000

    [Finish]2-6-2

---
+++ lesson6-2.rb
@@ -0,0 +1,18 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+repeat = gets.to_i
+
+for r in 1..repeat
+  puts "#{r}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a+b}"
+  puts "#{a}-#{b}=#{a-b}"
+  puts "#{a}*#{b}=#{a*b}"
+  puts "#{a}/#{b}=#{a/b}"
+end
+puts "計算を終了します"
