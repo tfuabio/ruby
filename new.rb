45e950868f524823020ef75135cbb225ae616642
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-10-24 08:02:27 +0000

    [Finish] 2-9 study

---
+++ new.rb
@@ -0,0 +1,19 @@
+class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロはしります。"
+  end
+
+end
+
+car = Car.new
+car.move("右", 5)
